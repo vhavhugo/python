e638a9af9dafbfa382622e46f6c129ef3476b3e6
vhavhugo <vhavhugo@hotmail.com>
2021-10-31 04:21:44 -0300

    Propriedades get e set

--- oo/cliente.py
+++ oo/cliente.py
@@ -1,3 +1,23 @@
 class Cliente:
     def __init__(self, nome):
-        self.nome = nome
+        self.__nome = nome
+
+    # se trata de uma propriedade, não precisa mais usar o ()
+    # exemplo: cliente.nome - o atributa precisa ser privado
+    # o metodo precisa ter o mesmo nome
+    # Terminal - Cmder
+    # from cliente import Cliente
+    # cliente = Cliente("Hugo")
+    # cliente.nome
+
+    @property 
+    def nome(self):
+        # .title() deixa a primeira maiuscula
+        print("chamando property nome()")
+        return self.__nome.title()
+
+    @nome.setter
+    def nome(self, nome):
+        print('Chamando setter name()')
+        self.__nome = nome
+        
--- oo/conta.py
+++ oo/conta.py
@@ -26,8 +26,10 @@ class Conta:
     def get_titular(self):
         return self.__titular
 
-    def get_limite(self):
+    @property
+    def limite(self):
         return self.__limite
 
-    def set_limit(self, limite):
+    @limite.setter
+    def limit(self, limite):
         self.__limite = limite
