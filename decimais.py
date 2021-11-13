e1cab4601894fe3d52a3f206140b044f4fbf3f63
vhavhugo <vhavhugo@hotmail.com>
2021-11-13 17:08:19 -0300

    decimais

--- decimais.py
+++ decimais.py
@@ -1,7 +1,14 @@
 from decimal import Decimal
 
-ganhos_julho = Decimal('99.91') * 5
+ganhos_julho = Decimal(99.91) * 5
 print(ganhos_julho)
 
-gastos_julho = Decimal('110.1') * 3
+gastos_julho = Decimal(110.1) * 3
 print(gastos_julho)
+
+# armazena_no_banco(ganhos_julho, gastos_julho)
+
+# ganhos_julho, gastos_julho = pega_valores_no_banco()
+
+# print(ganhos_junho / 100)
+# print(gastos_junho / 100)
