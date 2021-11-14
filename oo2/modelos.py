f0883303370b3aaacecdd2d4e388482993cb5eb0
vhavhugo <vhavhugo@hotmail.com>
2021-11-14 17:35:37 -0300

    arrumando arquivo

--- oo2/modelos.py
+++ oo2/modelos.py
@@ -8,37 +8,35 @@ class Programa:
     def likes(self):
         return self._likes
 
-    def dar_like(self):
+    def dar_likes(self):
         self._likes += 1
 
     @property
     def nome(self):
         return self._nome
 
-    #nome.setter
-    def nome(self, novo_nome):
-        self._nome = novo_nome.title()
-    
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
 
 class Filme(Programa):
-    # self.nome = nome.capitalize() # Primeira maiuscula
-    # self.nome = nome.title() # Todas as primeiras maiusculas
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
-        
-    def retorna_cadastro_diferenciado(self):
-        pass
-    
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao} - {vingadores.likes}')
 atlanta = Serie('atlanta', 2018, 2)
-atlanta.dar_like()
-atlanta.dar_like()
-print(f'{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas} - {atlanta.likes}')
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
