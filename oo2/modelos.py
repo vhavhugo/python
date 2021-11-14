b59fdac222f320a609ae599d602b71e49d2427e9
vhavhugo <vhavhugo@hotmail.com>
2021-11-14 18:21:47 -0300

    polimorfismo

--- oo2/modelos.py
+++ oo2/modelos.py
@@ -18,16 +18,27 @@ class Programa:
     @nome.setter
     def nome(self, nome):
         self._nome = nome
+        
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.likes} Likes'
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self.likes} Likes'
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} tempradas - {self.likes} Likes'
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -38,5 +49,12 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+# print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+# print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    #if de uma linha # detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(programa)
+        
